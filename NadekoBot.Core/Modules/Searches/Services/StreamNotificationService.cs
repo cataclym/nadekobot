671e2a70738e7941df44474fd0fa103aa00afa50
Alan Beatty <alanbeatty@hey.com>
2020-09-08 15:59:17 -0500

    removed viewers from stram notifs

--- NadekoBot.Core/Modules/Searches/Services/StreamNotificationService.cs
+++ NadekoBot.Core/Modules/Searches/Services/StreamNotificationService.cs
@@ -400,7 +400,6 @@ namespace NadekoBot.Modules.Searches.Services
                 /*.AddField(efb => efb.WithName(GetText(guildId, "viewers"))
                     .WithValue(status.IsLive ? status.Viewers.ToString() : "-")
                     .WithIsInline(true))*/
-                    
                 .WithColor(status.IsLive ? NadekoBot.OkColor : NadekoBot.ErrorColor);
 
             if (!string.IsNullOrWhiteSpace(status.Title))
