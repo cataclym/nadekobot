e78f4383d8ae3cd529a0f09aee426e35a76841c9
Alan Beatty <alanbeatty@hey.com>
2020-09-08 16:34:49 +0000

    Will not log other bot message deletions, removed total value of owned waifus from economy, pick confimations not deleted, and alias confirmations removed.

--- NadekoBot.Core/Modules/Gambling/Gambling.cs
+++ NadekoBot.Core/Modules/Gambling/Gambling.cs
@@ -73,7 +73,7 @@ namespace NadekoBot.Modules.Gambling
                 .AddField(GetText("currency_owned"), ((BigInteger)ec.Cash) + _bc.BotConfig.CurrencySign)
                 .AddField(GetText("currency_one_percent"), (onePercent * 100).ToString("F2") + "%")
                 .AddField(GetText("currency_planted"), ((BigInteger)ec.Planted) + _bc.BotConfig.CurrencySign)
-                .AddField(GetText("owned_waifus_total"), ((BigInteger)ec.Waifus) + _bc.BotConfig.CurrencySign)
+                //.AddField(GetText("owned_waifus_total"), ((BigInteger)ec.Waifus) + _bc.BotConfig.CurrencySign)
                 .AddField(GetText("bot_currency"), ec.Bot + _bc.BotConfig.CurrencySign)
                 .AddField(GetText("total"), ((BigInteger)(ec.Cash + ec.Bot + ec.Planted + ec.Waifus)).ToString("N", _enUsCulture) + _bc.BotConfig.CurrencySign)
                 .WithOkColor();
