c885e572c8850512e2ca18c21fe6e78181fc4828
Cata <11278743+cataclym@users.noreply.github.com>
2021-09-30 23:01:02 +0200

    Re-added reactions module

---
+++ src/NadekoBot/Modules/Reactions/Reactions.cs
@@ -0,0 +1,93 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using NadekoBot.Common.Attributes;
+using NadekoBot.Extensions;
+using NadekoBot.Modules.Reactions.Common;
+using Newtonsoft.Json;
+
+namespace NadekoBot.Modules.Reactions
+{
+    public partial class Reactions : NadekoModule
+    {
+        private readonly IHttpClientFactory _httpFactory;
+
+        public Reactions(IHttpClientFactory factory)
+        {
+            _httpFactory = factory;
+        }
+
+        [NadekoCommand, Usage, Description, Aliases]
+        public async Task hug([Leftover] string text = null)
+        {
+            var emb = _eb.Create()
+                .WithOkColor();
+
+            await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
+
+            using (var http = _httpFactory.CreateClient())
+            {
+                var img = await http.GetAsync("https://waifu.pics/api/sfw/hug").ConfigureAwait(false);
+                var content = await img.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<WaifuData>(content);
+                if (data != null) emb.WithImageUrl(data.URL);
+            }
+
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                emb.WithDescription($"{ctx.User.Mention} hugged {text}");
+            }
+
+            await ctx.Channel.EmbedAsync(emb);
+        }
+
+        [NadekoCommand, Usage, Description, Aliases]
+        public async Task pat([Leftover] string text = null)
+        {
+            var emb = _eb.Create()
+                .WithOkColor();
+
+            await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
+
+            using (var http = _httpFactory.CreateClient())
+            {
+                var img = await http.GetAsync("https://waifu.pics/api/sfw/pat").ConfigureAwait(false);
+                var content = await img.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<WaifuData>(content);
+                if (data != null) emb.WithImageUrl(data.URL);
+            }
+
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                emb.WithDescription($"{ctx.User.Mention} patted {text}");
+            }
+
+            await ctx.Channel.EmbedAsync(emb);
+        }
+
+        [NadekoCommand, Usage, Description, Aliases]
+        public async Task kiss([Leftover] string text = null)
+        {
+            var emb = _eb.Create()
+                .WithOkColor();
+
+            await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
+
+            using (var http = _httpFactory.CreateClient())
+            {
+                var img = await http.GetAsync("https://waifu.pics/api/sfw/kiss").ConfigureAwait(false);
+                var content = await img.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<WaifuData>(content);
+                if (data != null) emb.WithImageUrl(data.URL);
+            }
+
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                emb.WithDescription($"{ctx.User.Mention} kissed {text}");
+            }
+
+            await ctx.Channel.EmbedAsync(emb);
+        }
+    }
+}
