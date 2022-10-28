29b6a822bd25b61c46531dc2bcdeaa3997b7f8eb
Kwoth <master.kwoth@gmail.com>
2022-10-28 15:15:07 +0200

    Started outline of todo commands

---
+++ src/NadekoBot/Modules/Utility/Todo/TodoCommands.cs
@@ -0,0 +1,24 @@
+namespace NadekoBot.Modules.Utility.Todo;
+
+public partial class Utility
+{
+    [Group("Todo")]
+    [Name("todo")]
+    public partial class TodoCommands
+    {
+        public async Task TodoAdd()
+        {
+
+        }
+
+        public async Task TodoRemove()
+        {
+            
+        }
+
+        public async Task TodoList()
+        {
+            
+        }
+    }
+}
